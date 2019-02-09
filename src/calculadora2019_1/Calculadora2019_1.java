cc384d5142b9896e5cf0d99efacbb84ac305e9e4
Sandra Cañas <Sandra Caas@DESKTOP-DUAQ3FH>
2019-02-09 18:20:24 -0500

    Se creó la documentacion y se generó java doc en calculadora2019_1\dist\javadoc\index.html

--- src/calculadora2019_1/Calculadora2019_1.java
+++ src/calculadora2019_1/Calculadora2019_1.java
@@ -1,68 +1,108 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package calculadora2019_1;
 
 /**
  *
  * @author Sandra Cañas
  */
+/**
+ * Esta clase simula el comportamiento de la calculadora con las funcionalidades
+ * basicas
+ */
 public class Calculadora2019_1 {
+
     private int ans;
-    public Calculadora2019_1(){
-        ans=0;
+
+    public Calculadora2019_1() {
+        ans = 0;
     }
-    
-    public int suma(int a, int b){
-        ans=a+b;
+
+    /**
+     *
+     * @param a recibe primer sumando
+     * @param b recibe segundo sumando
+     * @return devuelve el total
+     */
+    public int suma(int a, int b) {
+        ans = a + b;
         return ans;
     }
-    
-    public int resta(int a, int b){
-        ans=a-b;
+
+    /**
+     *
+     * @param a minuendo
+     * @param b sustraendo
+     * @return devuelve la diferencia
+     */
+    public int resta(int a, int b) {
+        ans = a - b;
         return ans;
     }
-    
-    public int adicionar(int v){
-        ans=ans+v;
+
+    /**
+     *
+     * @param v primer factor
+     * @param b segundo factor
+     * @return producto
+     */
+    public int multiplicar(int v, int b) {
+        ans = v * b;
         return ans;
     }
-    
-    public int multiplicar(int v,int b){
-        ans=v*b;
-        return ans;
-    }
-    
-    public int sustraer(int v){
-        ans=ans-v;
-        return ans;
-    }
-    
-    public int devolver_ans(){
-        return ans;
-    }
-    
-    public void clear(){
-        ans=0;
-    }
-    
-    public float dividir(int a, int b){
-        if (b==0){
+
+    /**
+     *
+     * @param a dividendo
+     * @param b divisor
+     * @return cociente
+     */
+    public float dividir(int a, int b) {
+        if (b == 0) {
             throw new ArithmeticException("no se puede dividir por cero");
         }
-        ans=a/b;
+        ans = a / b;
         return ans;
     }
-    
-    public int potencia (int b,int e){
-    ans=1;
-    for (int i=0; i< e;i++){
-        
-        ans=ans*b;
-    }
-    return ans;
-}
-}
 
+    /**
+     *
+     * @param b base
+     * @param e exponente
+     * @return potencia
+     */
+    public int potencia(int b, int e) {
+        ans = 1;
+        for (int i = 0; i < e; i++) {
+
+            ans = ans * b;
+        }
+        return ans;
+    }
+
+    /**
+     *
+     * @param v numero que se va a adicionar al acumulado
+     * @return
+     */
+    public int adicionar(int v) {
+        ans = ans + v;
+        return ans;
+    }
+
+    /**
+     *
+     * @param v numero que se va a sustraer al acumulado
+     * @return
+     */
+    public int sustraer(int v) {
+        ans = ans - v;
+        return ans;
+    }
+
+//
+//    public int devolver_ans() {
+//        return ans;
+//    }
+    public void clear() {
+        ans = 0;
+    }
+}
